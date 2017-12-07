47f97636b1347b1690e55f0a7f98b92da3048b83
chengong <654450188@qq.com>
2017-12-08 01:04:04 +0800

    anydoor callback version

--- src/app.js
+++ src/app.js
@@ -1,21 +1,41 @@
 const http = require('http');
-const {port, hostname} = require('./config/defaultConfig');
+const path = require('path');
+const fs = require('fs');
+const {port, hostname, root} = require('./config/defaultConfig');
 const chalk = require('chalk');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': "text/html; charset=utf8"
+  /*获取用户的访问的 url*/
+  let {url} = req;
+  /*用根路径与用户访问的 url 拼接成绝对路径*/
+  let filePath = path.join(root, url);
+
+  /*判断是文件还是文件夹*/
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      res.writeHead(404, 'error', {
+        'Content-Type': 'text/html; charset=utf8'
+      });
+      res.end(`${filePath} 不找到文件或文件夹`);
+      return;
+    }
+    if (stats.isDirectory()) {
+      /*如果是文件夹，则给出文件列表*/
+      fs.readdir(filePath, (err, files) => {
+        res.writeHead(200, 'directory', {
+          'Content-Type': 'text/plain; charset=utf8'
+        });
+        res.end(files.join('\n'));
+      });
+    }
+    if (stats.isFile()) {
+      /*如果是文件，则返回文件内容*/
+      res.writeHead(200, 'file', {
+        'Content-Type': 'text/plain; charset=utf8'
+      });
+      fs.createReadStream(filePath).pipe(res);
+    }
   });
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.write('<h1>标题1</h1>');
-  res.end();
 });
 
 server.listen(port, hostname, () => {
--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,4 +1,5 @@
 const defaultConfig = {
+  root: process.cwd(),
   hostname: '127.0.0.1',
   port: 9527
 };
