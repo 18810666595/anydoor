120d5b319ed4a44393eb07f11887f7bf6c891e15
chengong <654450188@qq.com>
2017-12-12 22:40:37 +0800

    terminal cli 可以使用 node src/index.js -p -d -h

--- src/app.js
+++ src/app.js
@@ -10,15 +10,17 @@ class Server {
   }
 
   start() {
+    const {root,port,hostname}=this.conf;
+
     const server = http.createServer((req, res) => {
       /*用根路径与用户访问的 url 拼接成绝对路径*/
-      let filePath = path.join(this.conf.root, req.url);
+      let filePath = path.join(root, req.url);
       /*根据用户访问的的绝对路径，匹配不同的路由，返回不同的结果*/
       route(req, res, filePath, this.conf);
     });
 
-    server.listen(this.conf.port, this.conf.hostname, () => {
-      let url = `http://${this.conf.hostname}:${this.conf.port}`;
+    server.listen(port, hostname, () => {
+      let url = `http://${hostname}:${port}`;
       console.log(`打开浏览器，访问 ${chalk.green(url)}`);
     });
 
